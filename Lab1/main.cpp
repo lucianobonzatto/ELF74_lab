af69b7574a3e5592df253fb175c822cf37bf3da6
lucianobonzatto <37905647+lucianobonzatto@users.noreply.github.com>
2022-03-08 11:24:40 -0300

    Initial commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,87 @@
+/*__________________________________________________________________________________
+|       Disciplina de Sistemas Embarcados - 2022-1
+|       Prof. Douglas Renaux
+| __________________________________________________________________________________
+|
+|		Lab 1
+| __________________________________________________________________________________
+*/
+
+/**
+ * @file     main.cpp
+ * @author   Douglas P. B. Renaux
+ * @brief    Solution to Lab1 of ELF74/CSW41 - UTFPR. \n 
+ *           Tools instalation and validation procedure.\n 
+ *           Show messages on terminal using std::cout. \n 
+ *           Show current value of some predefined macros (preprocessor symbols).\n 
+ *           Read float value from terminal using std::cin.
+ * @version  V2 -for 2022-1 semester
+ * @date     Feb, 2022
+ ******************************************************************************/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Use Doxygen to report lab results
+ *
+ *------------------------------------------------------------------------------*/
+/** @mainpage Results from Lab1
+ *
+ * @section Ouput Values
+ *
+ * The values of ...
+ *
+ * @section Terminal
+ *
+ * @subsection Output
+ *
+ * etc...
+ */
+
+/*------------------------------------------------------------------------------
+ *
+ *      File includes
+ *
+ *------------------------------------------------------------------------------*/
+#include <stdint.h>
+
+#include <iostream>
+using std::cout;
+
+#include "template.h"
+
+/*------------------------------------------------------------------------------
+ *
+ *      Typedefs and constants
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Global vars
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      File scope vars
+ *
+ *------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------
+ *
+ *      Functions and Methods
+ *
+ *------------------------------------------------------------------------------*/
+/**
+ * Main function.
+ *
+ * @param[in] argc - not used, declared for compatibility
+ * @param[in] argv - not used, declared for compatibility
+ * @returns int    - not used, declared for compatibility
+ */
+int main(int argc, char ** argv)
+{
+
+ 
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Repository template to be used by ELF74/CSW41 students
+
+Lab1 - Tiva board without TivaWare 
+
+Lab2 - Tiva board with TivaWare 
