9eee2373165b166d2d5d8bcff4142a7deb81d15c
Hadryan Salles <hadryansalles00@gmail.com>
2022-03-15 23:10:24 -0300

    added lab2 game with single round

--- Labs/Hadryan_e_Luciano_Lab2/src/hal_entry.c
+++ Labs/Hadryan_e_Luciano_Lab2/src/hal_entry.c
@@ -21,14 +21,34 @@
 * Description  : This is a very simple example application that blinks all LEDs on a board.
 ***********************************************************************************************************************/
 
-#include <stdio.h>
 #include "hal_data.h"
+#include <stdio.h>
 
-void timerCallback(timer_callback_args_t *p_args){
+extern void initialise_monitor_handles(void);
+
+void setLeds(bsp_leds_t leds, ioport_level_t level);
+
+volatile int timerSet = 0;
+volatile int switchSet = 0;
+
+void timerCallback(timer_callback_args_t *p_args) {
     (void) p_args;
-    printf("oi\n");
+    timerSet = 1;
+    printf("Timer Set\n");
 }
 
+void switchCallback(external_irq_callback_args_t *p_args) {
+    (void) p_args;
+    switchSet = 1;
+    printf("Switch Set\n");
+}
+
+void setLeds(bsp_leds_t leds, ioport_level_t level) {
+    for(uint32_t i = 0; i < leds.led_count; i++)
+    {
+        g_ioport.p_api->pinWrite(leds.p_leds[i], level);
+    }
+}
 
 /*******************************************************************************************************************//**
  * @brief  Blinky example application
@@ -38,48 +58,45 @@ void timerCallback(timer_callback_args_t *p_args){
  *
  **********************************************************************************************************************/
 void hal_entry(void) {
+    initialise_monitor_handles();
 
-	/* Define the units to be used with the software delay function */
-	const bsp_delay_units_t bsp_delay_units = BSP_DELAY_UNITS_MILLISECONDS;
-	/* Set the blink frequency (must be <= bsp_delay_units */
-    const uint32_t freq_in_hz = 2;
-	/* Calculate the delay in terms of bsp_delay_units */
-    const uint32_t delay = bsp_delay_units/freq_in_hz;
-	/* LED type structure */
+	// const bsp_delay_units_t bsp_delay_units = BSP_DELAY_UNITS_MILLISECONDS;
+	// const uint32_t freq_in_hz = 1;
+	// const uint32_t delay = bsp_delay_units/freq_in_hz;
+    // R_BSP_SoftwareDelay(delay, bsp_delay_units);
+
     bsp_leds_t leds;
-	/* LED state variable */
-    ioport_level_t level = IOPORT_LEVEL_HIGH;
+	R_BSP_LedsGet(&leds);
 
-    /* Get LED information for this board */
-    R_BSP_LedsGet(&leds);
+    // setup timer interruption
+    g_timer0.p_api->open(g_timer0.p_ctrl, g_timer0.p_cfg);
+    g_timer0.p_api->reset(g_timer0.p_ctrl);
+    // setup switch button interruption
+    g_external_irq0.p_api->open(g_external_irq0.p_ctrl, g_external_irq0.p_cfg);
 
-    open(g_timer0.p_ctrl, g_timer0.p_cfg);
+    setLeds(leds, IOPORT_LEVEL_HIGH);
 
-    /* If this board has no LEDs then trap here */
-    if (0 == leds.led_count)
-    {
-        while(1);   // There are no LEDs on this board
+    printf("Starting the game...\n");
+
+    // wait initial second
+    while(!timerSet)
+    {}
+
+    setLeds(leds, IOPORT_LEVEL_LOW);
+
+    // start timer to 3 seconds
+    g_timer0.p_api->periodSet(g_timer0.p_ctrl, 3, TIMER_UNIT_PERIOD_SEC);
+    g_timer0.p_api->start(g_timer0.p_ctrl);
+    timerSet = 0;
+
+    while(!timerSet) {
+        if(switchSet) {
+            switchSet = 0;
+            unsigned long int timeCount;
+            g_timer0.p_api->counterGet(g_timer0.p_ctrl, &timeCount);
+            printf("You win! Counter: %lf s\n", timeCount/120000000.0f);
+        }
     }
 
-    while(1)
-    {
-        /* Determine the next state of the LEDs */
-        if(IOPORT_LEVEL_LOW == level)
-        {
-            level = IOPORT_LEVEL_HIGH;
-        }
-        else
-        {
-            level = IOPORT_LEVEL_LOW;
-        }
-
-        /* Update all board LEDs */
-        for(uint32_t i = 0; i < leds.led_count; i++)
-        {
-            g_ioport.p_api->pinWrite(leds.p_leds[i], level);
-        }
-
-        /* Delay */
-        R_BSP_SoftwareDelay(delay, bsp_delay_units);
-    }
+    // R_BSP_SoftwareDelay(delay, bsp_delay_units);
 }
--- Labs/Hadryan_e_Luciano_Lab2/src/synergy_gen/hal_data.h
+++ Labs/Hadryan_e_Luciano_Lab2/src/synergy_gen/hal_data.h
@@ -4,10 +4,17 @@
 #include <stdint.h>
 #include "bsp_api.h"
 #include "common_data.h"
+#include "r_icu.h"
+#include "r_external_irq_api.h"
 #include "r_gpt.h"
 #include "r_timer_api.h"
 #ifdef __cplusplus
 extern "C" {
+#endif
+/* External IRQ on ICU Instance. */
+extern const external_irq_instance_t g_external_irq0;
+#ifndef switchCallback
+void switchCallback(external_irq_callback_args_t *p_args);
 #endif
 /** Timer on GPT Instance. */
 extern const timer_instance_t g_timer0;
